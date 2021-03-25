bab2c93ea00461a18d1a553e2018cc095cdf2c2f
delphdevweb <74188283+delphdevweb@users.noreply.github.com>
2021-03-25 22:28:49 +0100

    Update footer.php

--- lequilibrerestaurationnew/partials-front/footer.php
+++ lequilibrerestaurationnew/partials-front/footer.php
@@ -2,6 +2,7 @@
    
        
         <div class="container text-center footer-grid social">
+         <div class="center">
             <ul>
                 <li>
                     <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
@@ -13,7 +14,8 @@
                 <a href="#"><img src="images/mail.svg" class='mail'/></a>
                 </li>
             </ul>
-            <p class='adresse'>1866 route de Villefort,</br>Saint Martin de Valguagues</br>06 51 48 51 27</p>
+             </div>
+            <p class='adresse'>1866 route de Villefort,</br>Saint Martin de Valgalgues</br>06 51 48 51 27</p>
             <p class='mentions'><a href="charte.html">Copyright 2021 l'Equilibre Restauration Mentions légales Designed By Sarah Diom, Lindsay Chognon and Delphine Robert</a></p>
             <p class='ouverture'>Nous sommes ouverts</br>Du mardi au dimanche</br> De 9h à 22h</p>
         </div>
